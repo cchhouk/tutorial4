a42021e24b8001869ad8daddc8ba1e0e3267dcb5
Christopher Chhouk <cvchhouk@gmail.com>
2022-02-07 19:46:18 -0800

    additional exercises

--- js/ex1.js
+++ js/ex1.js
@@ -3,3 +3,22 @@
 */
 
 console.log("Chhouk's Output from Tutorial 4 Example 1");
+
+let nameEntered = String(prompt("Enter your name:"));
+let gradeStanding = Number(prompt("Enter the number of units you've completed:"))
+
+if (gradeStanding > 91) {
+   let gradeStanding = 'Senior'; 
+   console.log(`Hello ${nameEntered}\nYour grade standing is ${gradeStanding}`);
+} else if ((gradeStanding >= 61) && (gradeStanding <= 90)) {
+   let gradeStanding = 'Junior'; 
+   console.log(`Hello ${nameEntered}\nYour grade standing is ${gradeStanding}`);
+} else if ((gradeStanding >= 31) && (gradeStanding <= 60)) {
+   let gradeStanding = 'Sophomore'; 
+   console.log(`Hello ${nameEntered}\nYour grade standing is ${gradeStanding}`);
+} else if ((gradeStanding >= 0) && (gradeStanding <= 30)) {
+   let gradeStanding = 'Freshman'; 
+   console.log(`Hello ${nameEntered}\nYour grade standing is ${gradeStanding}`);
+} else {
+   console.log(`Hello ${nameEntered}, you entered an incorrect value.`);
+}
--- js/ex2.js
+++ js/ex2.js
@@ -3,3 +3,40 @@
 */
 
 console.log("Chhouk's Output from Tutorial 4 Example 2");
+
+let dayEntered = (prompt("Enter the day of the week using its three letter abbreviation:"));
+let followingDay = "placeholder";
+
+switch (dayEntered) {
+   case "sun":
+      followingDay = "mon";
+      console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`)
+      break;
+   case "mon":
+      followingDay = "tue";
+      console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`)
+      break;
+   case "tue":
+      followingDay = "wed";
+      console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`)
+      break;
+   case "wed":
+      followingDay = "thur";
+      console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`)
+      break;
+   case "thur":
+      followingDay = "fri";
+      console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`)
+      break;
+   case "fri":
+      followingDay = "sat";
+      console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`)
+      break;
+   case "sat":
+      followingDay = "sun";
+      console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`)
+      break;
+   default:
+      console.log("You did not enter a valid day of the week")
+};
+
